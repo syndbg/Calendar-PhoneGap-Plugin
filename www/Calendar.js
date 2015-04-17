fe10208d7662ebee9d1e1fe22a30c6b1ba83eab2
aantonov <aantonov@asteasolutions.com>
2015-04-17 15:54:56 +0300

    Added createEventInteractivelyWithOptions

--- www/Calendar.js
+++ www/Calendar.js
@@ -91,7 +91,11 @@ Calendar.prototype.createEvent = function (title, location, notes, startDate, en
   Calendar.prototype.createEventWithOptions(title, location, notes, startDate, endDate, {}, successCallback, errorCallback);
 };
 
-Calendar.prototype.createEventInteractively = function (title, location, notes, startDate, endDate, calOptions, successCallback, errorCallback) {
+Calendar.prototype.createEventInteractively = function (title, location, notes, startDate, endDate, successCallback, errorCallback) {
+  Calendar.prototype.createEventInteractivelyWithOptions(title, location, notes, startDate, endDate, null, successCallback, errorCallback);
+};
+
+Calendar.prototype.createEventInteractivelyWithOptions = function (title, location, notes, startDate, endDate, calOptions, successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, "Calendar", "createEventInteractively", [{
     "title": title,
     "location": location,
@@ -101,6 +105,7 @@ Calendar.prototype.createEventInteractively = function (title, location, notes, 
     "options": calOptions instanceof Object ? calOptions : Calendar.prototype.getCalendarOptions()
   }]);
 };
+
 
 // TODO add calendarname to options and call that method, like we did with createEvent
 Calendar.prototype.createEventInNamedCalendar = function (title, location, notes, startDate, endDate, calendarName, successCallback, errorCallback) {
@@ -115,10 +120,6 @@ Calendar.prototype.createEventInNamedCalendar = function (title, location, notes
     "endTime": endDate instanceof Date ? endDate.getTime() : null,
     "calendarName": calendarName
   }]);
-};
-
-Calendar.prototype.createEventInIdCalendar = function (title, location, notes, startDate, endDate, calendarId, successCallback, errorCallback) {
-  Calendar.prototype.createEventWithOptions(title, location, notes, startDate, endDate, {"calendarId": calendarId}, successCallback, errorCallback);
 };
 
 Calendar.prototype.deleteEvent = function (title, location, notes, startDate, endDate, successCallback, errorCallback) {
